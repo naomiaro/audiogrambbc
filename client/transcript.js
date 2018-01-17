0dc3246f0b7a1c8abe3ea4ce9be02ae01fadbe9b
Jonty Usborne <jonty.usborne@bbc.co.uk>
2018-01-17 16:41:59 +0000

    Initialise timing wizard

--- client/transcript.js
+++ client/transcript.js
@@ -620,6 +620,24 @@ function importFromFile() {
     reader.readAsText(this.files[0]);
 }
 
+function loadTimingsEditor() {
+    var subs = toSubs();
+    var maxWidth = 0;
+    subs.forEach(function(sub) {
+        var block = jQuery('#transcript-timings .block.template').clone().removeClass('template');
+        var text = '';
+        sub.lines.forEach(function(line) {
+            if (text.length) text += '<br/>';
+            text += line;
+        });
+        block.find('.subtitles').html(text);
+        jQuery('#transcript-timings .block:last').after(block);
+        var width = jQuery("#transcript-timings .block:last .subtitles").width() + 5;
+        if (width > maxWidth) maxWidth = width;
+    });
+    jQuery("#transcript-timings .block .subtitles").width(maxWidth);
+}
+
 function init() {
     // Attach listener to hightlight words during playback
     jQuery('audio').on('timeupdate', function() {
@@ -985,6 +1003,7 @@ function init() {
 
     jQuery(document).on('click', '#transcript-btn-timings', function(){
         jQuery("#transcript").toggleClass("timings");
+        loadTimingsEditor();
     });
     
 }
