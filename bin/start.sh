dcd0b32e08b63f4403693988ca23b2ca961fcc69
Jonty Usborne <jonty.usborne@bbc.co.uk>
2018-02-13 17:46:48 +0000

    Typo

--- bin/start.sh
+++ bin/start.sh
@@ -12,7 +12,8 @@ else
   echo "Starting local redis"
   nohup redis-server --dir /home/audiogram/redis --appendonly yes --logfile redis.log &
 fi
-npm run debug | adddate &>> console.log
 
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
-curl --noproxy '*' -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" -d "{ \"what\": \"Audiogram - deploy\", \"tags\": [\"audiogram\", \"start\"], \"data\": \"[SERVICE STARTED] v$PACKAGE_VERSION\" }" 'http://audiogram.newslabs.co:8081/events/'
+curl --noproxy '*' -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" -d "{ \"what\": \"Audiogram - start\", \"tags\": [\"audiogram\", \"start\"], \"data\": \"[SERVICE STARTED] v$PACKAGE_VERSION\" }" 'http://audiogram.newslabs.co:8081/events/'
+
+npm run debug | adddate &>> console.log
