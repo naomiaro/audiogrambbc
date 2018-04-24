840bb7200c5b70e11158f221fc8c7c2a522a4af0
Jonty Usborne <jonty.usborne@bbc.co.uk>
2018-04-24 15:25:45 +0100

    Link header home

--- client/ui.js
+++ client/ui.js
@@ -108,6 +108,9 @@ function sizeSliders() {
 }
 
 function init(cb) {
+    jQuery(document).on("click", "#header-home", function () {
+        window.location.href = '/';
+    });
     initializeSliders();
     sizeButtonSelect();
     sizeSliders();
