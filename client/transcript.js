bdc66d07eb30a601cfa585503705881e081f470b
Jonty Usborne <jonty.usborne@gmail.com>
2018-04-29 19:11:06 +0100

    Add kaldi timer

--- client/transcript.js
+++ client/transcript.js
@@ -653,6 +653,7 @@ function generate(blob) {
         success: function(data){
             kaldiTimer = Date.now();
             poll(data.job);
+            // startTimer();
         },
         error: function(jqXHR, textStatus, errorThrown){
             jQuery("#transcript-pane .error span").text(errorThrown + " (" + jqXHR.status + "): " + jqXHR.responseText);
@@ -661,6 +662,31 @@ function generate(blob) {
         }
     });
     
+}
+
+function startTimer() {
+    // Trendline: y=0.351x+126
+    var duration = audio.duration();
+    var estimate = (0.351*duration)+130;
+    var end = (kaldiTimer/1000) + estimate;
+    updateTimer(end);
+}
+
+function updateTimer(end){
+    var now = Date.now();
+    var remaining = end - (now/1000);
+    if (remaining > 0) {
+        var hms = utils.formatHMS(remaining).split('.')[0];
+        var text = `Estimated time remaining: ${hms}`;
+    } else {
+        var text = "Sorry this job is taking a little longer than average. Shouldn't be much longer!";
+    }
+    jQuery('#transcript-pane .loading .estimate').text(text);
+    if (remaining > 0) {
+        setTimeout(() => {
+            updateTimer(end);
+        }, 1000);
+    }
 }
 
 function exportTranscript() {
